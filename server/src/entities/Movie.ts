f08d71003599fe5d37eda77d3ae06d12217d66a6
zhangzhuoya <1114976368@qq.com>
2021-10-19 17:27:01 +0800

    处理plainobject的转换

--- server/src/entities/Movie.ts
+++ server/src/entities/Movie.ts
@@ -1,33 +1,44 @@
-import { ArrayMinSize, IsInt, IsNotEmpty, Max, Min, MinLength } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ArrayMinSize, IsArray, IsInt, IsNotEmpty, Max, Min, MinLength } from 'class-validator';
 
 export class Movie {
     @IsNotEmpty({message: "电影名称不可以为空"})
+    @Type(()=> String)
     public name: string;
 
     @IsNotEmpty({message: "电影类型不可以为空"})
     @MinLength(1,{message: "电影类型至少有一个"})
     @ArrayMinSize(1,{message: '数组最小长度为1'})
+    @IsArray({message:"必须是数组"})
+    @Type(()=> String)
     public types: string [];
 
     @IsNotEmpty({message: "上映地区不可以为空"})
     @MinLength(1,{message: "上映地区至少有一个"})
     @ArrayMinSize(1,{message: '数组最小长度为1'})
+    @Type(()=> String)
     public areas: string [];
 
     @IsNotEmpty({message: "时长不可以为空"})
     @IsInt({message: '时长必须是整数'})
     @Min(1,{message: "时长最少一分钟"})
     @Max(1,{message: "时长过长"})
+    @Type(()=> Number)
     public timeLong: number;
 
     @IsNotEmpty({message: "是否热映不可以为空"})
+    @Type(()=> Boolean)
     public isHot: boolean
 
     @IsNotEmpty({message: "是否为经典影片不可以为空"})
+    @Type(()=> Boolean)
     public isClasic: boolean
 
     @IsNotEmpty({message: "描述信息不可以为空"})
+    @Type(()=> Boolean)
     public desc: boolean
+
     @IsNotEmpty({message: "图片背景不能为空"})
+    @Type(()=> String)
     public poster: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
-// 数据库
+import "reflect-metadata"
 import { Movie } from "./entities/Movie";
 import { validate } from "class-validator";
-const m = new Movie();
-m.types=['dd']
-validate(m).then(res=>{
+import { plainToClass } from 'class-transformer'
+const m:object = {};
+// m.types=['dd']
+// 将plain Object转化为movie对象
+const movie = plainToClass(Movie, m as object)
+console.log(movie);
+
+validate(movie).then(res=>{
     console.log(res);
 })
 // 将 plain Object转化为
